1f135a31ba4a876c6d2892be3b2a85ce93249fc4
szinuccsi <szinyeribence@gmail.com>
2021-04-10 21:11:38 +0200

    Branch-A pszeudo változtatás

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -37,7 +37,7 @@ public class GT4500 implements SpaceShip {
   public boolean fireTorpedo(FiringMode firingMode) {
 
     boolean firingSuccess = false;
-
+    firingSuccess = false;
     switch (firingMode) {
       case SINGLE:
         if (wasPrimaryFiredLast) {
